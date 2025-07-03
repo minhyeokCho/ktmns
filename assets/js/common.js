1b8345931faedee06a61b2a7145418004c356ea5
minhyeokCho <whalsgur0000@naver.com>
2025-07-03 21:22:46 +0900

    커밋

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-	$('#header').length && h_menu() 
+	$('#header').length && menu() 
+	$('.family').length && familySite() 
 	$('.select_box').niceSelect()
 });
 
@@ -8,4 +9,32 @@ function dimShow(){ /* 딤드 show */
 }
 function dimHide(){ /* 딤드 hide */
     $('body').removeClass('dim');
+}
+
+$(function () {
+
+});
+
+function menu() {
+	const $dept2 = $('.dept_02');
+	const $menuBg = $('.menu_bg');
+
+	$('.dept_01 > li').mouseenter(function () {
+		$dept2.stop(true, true).slideDown(300);
+		$menuBg.stop(true, true).slideDown(300);
+	});
+
+	$('.h_btm').mouseleave(function () {
+		if (!$('.dept_01 > li:hover, .dept_02:hover, .menu_bg:hover').length) {
+			$dept2.stop(true, true).slideUp(300);
+			$menuBg.stop(true, true).slideUp(300);
+		}
+	});
+}
+
+function familySite() {
+	$('.family button').on('click', function () {
+		$('.family').toggleClass('active')
+		$('.family ul').stop().slideToggle(300)
+	})
 }
